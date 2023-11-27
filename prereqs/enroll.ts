d8743919279878d2e548dfa5cbace71a02294152
nurs.syhn <nursultan.seyhan@gmail.com>
2023-11-27 12:14:26 +0300

    organize

--- prereqs/enroll.ts
+++ prereqs/enroll.ts
@@ -2,7 +2,7 @@ import { Connection, Keypair, SystemProgram, PublicKey } from
     "@solana/web3.js"
 import { Program, Wallet, AnchorProvider, Address } from
     "@project-serum/anchor"
-import { WbaPrereq, IDL } from "./programs/wba_prereq";
+import { WbaPrereq, IDL } from "./wba_prereq";
 import wallet from "./wba-wallet.json"
 
 // We're going to import our keypair from the wallet file
