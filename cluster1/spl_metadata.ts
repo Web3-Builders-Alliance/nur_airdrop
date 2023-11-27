f673bed8ac3cde798a1a98fd303e70a2441e6d12
nurs.syhn <nursultan.seyhan@gmail.com>
2023-11-27 11:41:01 +0300

    cluster1

---
+++ cluster1/spl_metadata.ts
@@ -0,0 +1,47 @@
+import { Commitment, Connection, Keypair, PublicKey, Transaction, sendAndConfirmTransaction } from "@solana/web3.js"
+import wallet from "./wba-wallet.json"
+import { createCreateMetadataAccountV2Instruction, createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+
+// We're going to import our keypair from the wallet file
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+
+//Create a Solana devnet connection
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
+
+// Define our Mint address
+const mint = new PublicKey("<mint address>")
+
+// Add the Token Metadata Program
+const token_metadata_program_id = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
+
+// Create PDA for token metadata
+const metadata_seeds = [
+    Buffer.from('metadata'),
+    token_metadata_program_id.toBuffer(),
+    mint.toBuffer(),
+];
+const [metadata_pda, _bump] = PublicKey.findProgramAddressSync(metadata_seeds, token_metadata_program_id);
+
+(async () => {
+    try {
+        // Start here
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
+
+let tx = await createMetadataAccountV3(
+    {}, {
+    createMetadataAccountArgsV3: {
+      metadata: mint3Metadata,
+      mint: mint3,
+      mintAuthority: keypair.publicKey,
+      payer: keypair.publicKey,
+      updateAuthority: keypair.publicKey,
+    },
+    createMetadataAccountArgsV3: {
+        data: {
+            name: "WBA",
+            symbol: "WBA",
+            uri: "https://arweave.net/euAlBrhc3NQJ5Q-oJnP10vsQFjTV7E9CgHZcVm8cogo",
