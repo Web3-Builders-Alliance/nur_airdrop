5eb72d1f3494bc521b4ea911130569eac7ae5eff
Nur Sultan Seyhanlioglu <nurssyhn@gmail.com>
2023-12-03 17:46:07 +0300

    Create wba_vault

--- ts/cluster1/vault_deposit.ts
+++ ts/cluster1/vault_deposit.ts
@@ -13,7 +13,7 @@ import {
   Address,
   BN,
 } from "@coral-xyz/anchor";
-import { WbaVault, IDL } from "../programs/wba_vault.ts";
+import { WbaVault, IDL } from "../programs/wba_vault";
 import wallet from "./wba-wallet.json";
 
 // Import our keypair from the wallet file
